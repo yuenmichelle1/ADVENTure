a89c90f37d5dc4b3ee86d8e7577fb7ca37a55b0d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-12-03 18:04:03 -0600

    day3

---
+++ 2024/3/day3.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# example_part_1_input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+def part_one(input)
+  real_mul_instructions = input.scan(/mul\(\d{1,3},\d{1,3}\)/)
+  real_mul_instructions.sum do |instruction|
+    instruction.scan(/\d{1,3}/).map(&:to_i).reduce(1, :*)
+  end
+end
+
+def find_pattern_indices(pattern, input)
+  input.enum_for(:scan, pattern).map { Regexp.last_match.offset(0).first }
+end
+
+def part_two(input)
+  dont_indices = find_pattern_indices(/don't\(\)/, input)
+  do_indices = find_pattern_indices(/do\(\)/, input)
+
+  add_to_string = true
+  modified_input = input.chars.each_with_index.with_object([]) do |(char, i), arr|
+    if dont_indices.include? i
+      add_to_string = false
+    elsif do_indices.include? i
+      add_to_string = true
+    end
+
+    arr << char if add_to_string
+  end
+  part_one(modified_input.join)
+end
+
+puts part_one(File.read('./day3_input.txt'))
+puts part_two(File.read('./day3_input.txt'))
