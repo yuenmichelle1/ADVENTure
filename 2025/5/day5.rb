fd24e98c7fa793c6f866bd60b7130e251e32fabf
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2025-12-05 03:23:46 -0600

    day 5

---
+++ 2025/5/day5.rb
@@ -0,0 +1,65 @@
+example_input = File.read('./day5_example_input.txt')
+input = File.read('./day5_input.txt')
+
+def part_one(input)
+    id_ranges = []
+    avail_ids = []
+    fresh_count = 0
+
+    input.split.each do |line|
+        if line.include?('-')
+            lower, upper = line.split('-').map(&:to_i)
+            id_ranges << (lower..upper)
+        else
+            avail_ids << line&.to_i
+        end
+    end
+
+    avail_ids.each do |i|
+        fresh_count += 1 if id_ranges.any? { |id_range| id_range.include?(i) }
+    end
+    fresh_count
+end
+
+# puts part_one(example_input)
+
+lower_bounds = []
+upper_bounds = []
+input.split.each do |line|
+    if line.include?('-')
+        lower, upper = line.split('-').map(&:to_i)
+        lower_bounds << lower
+        upper_bounds << upper
+    end
+end
+
+lower_bounds = lower_bounds.sort
+upper_bounds = upper_bounds.sort
+new_lowers = []
+new_uppers = []
+lower_bounds.each_with_index do |lower, i|
+    if new_lowers.empty?
+        new_lowers << lower
+        new_uppers << upper_bounds[i]
+    else
+        recent_upper = new_uppers[-1]
+        if lower <= recent_upper
+            new_uppers.pop
+            new_uppers << upper_bounds[i]
+        else
+            new_lowers << lower
+            new_uppers << upper_bounds[i]
+        end
+    end
+end
+
+distances = 0
+new_lowers.each_with_index do |lower, i|
+    distances += (new_uppers[i] - lower + 1)
+end
+puts distances
+
+
+
+
+
